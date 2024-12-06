6a307fbb7b64a4c5749d5d6b843d75248b9c0578
brandonchen <zipengchen1832@gmail.com>
2024-12-06 13:28:40 -0500

    remove react temp

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Day2 from "./day2/Day2";
+import Day3 from "./day3/Day3";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Day2 />
+      {/* <Day2 /> */}
+      <Day3 />
     </>
   );
 }
