bf22e4583b4bcb47982477a1a605f6018de1c48f
wendywen20 <wenyimei6699@gmail.com>
2025-01-03 14:10:44 -0500

    addTodo

--- redux-live-coding/src/components/ToDoList.jsx
+++ redux-live-coding/src/components/ToDoList.jsx
@@ -1,27 +1,43 @@
-import { useSelector } from "react-redux";
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
 
 const ToDoList = () => {
+  const todos = useSelector((state) => state.todos);
+  const [inputValue, setInputValue] = useState("");
 
-    const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            {todos.map((todo) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = { id: 3, todoName: inputValue, completed: false };
+    dispatch({ type: "ADD_TODO", payload: newTodo });
+  };
 
-                const { id, todoName, completed } = todo;
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
 
-                return (
-                    <React.Fragment key={id}>                       
-                        <div>id: {id}</div>
-                        <div>name: {todoName}</div>
-                        <div>status: status: {completed? "completed" : "not completed"}</div>
-                    </React.Fragment>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} />
+        <button type="submit">Add newTodo</button>
+      </form>
+      {todos.map((todo) => {
+        const { id, todoName, completed } = todo;
 
-}
+        return (
+          <React.Fragment key={id}>
+            <div>id: {id}</div>
+            <div>name: {todoName}</div>
+            <div>
+              status: status: {completed ? "completed" : "not completed"}
+            </div>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
 
 export default ToDoList;
--- redux-live-coding/src/redux/reducers/todosReducer.js
+++ redux-live-coding/src/redux/reducers/todosReducer.js
@@ -1,9 +1,14 @@
-const InitialState = [{ id: 1, todoName: 'eat', completed: false }];
+const InitialState = [
+  { id: 1, todoName: "eat", completed: false },
+  { id: 2, todoName: "sleep", completed: false },
+];
 export default function todosReducer(state = InitialState, action) {
-    const { type, payload } = action;
-    switch (type) {
-        default:
-            return state;
-    }
-}
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD_TODO" :
+      return [...state, payload];
 
+    default:
+      return state;
+  }
+}
