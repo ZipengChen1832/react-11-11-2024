4e60b57830d47fed3877cb9b94690098b6d9cc13
brandonchen <zipengchen1832@gmail.com>
2025-01-03 12:02:41 -0800

    fix cursed file

--- redux-live-coding/src/components/TodoList.jsx
+++ redux-live-coding/src/components/TodoList.jsx
@@ -1,19 +1,43 @@
-import React,{useState} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-export default function TodoList() {
-    const todos=useSelector((state)=>state.todo)
-    const dispatch =useDispatch()
-    console.log(todos)
-    const [newTodo, setNewTodo]=useState('')
+import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+
+const ToDoList = () => {
+  const todos = useSelector((state) => state.todos);
+  const [inputValue, setInputValue] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = { id: 3, todoName: inputValue, completed: false };
+    dispatch({ type: "ADD_TODO", payload: newTodo });
+  };
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
-    <h1>Todo Component</h1>
-    <input type="text" onChange={(e)=>{setNewTodo(e.target.value)}}/> 
-    <button>Add Todo</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} />
+        <button type="submit">Add newTodo</button>
+      </form>
+      {todos.map((todo) => {
+        const { id, todoName, completed } = todo;
 
-    <div>
-      {todos?.map((todo)=>{todo.todoName})}
+        return (
+          <React.Fragment key={id}>
+            <div>id: {id}</div>
+            <div>name: {todoName}</div>
+            <div>
+              status: status: {completed ? "completed" : "not completed"}
+            </div>
+          </React.Fragment>
+        );
+      })}
     </div>
-    </div>
-  )
-}
+  );
+};
+
+export default ToDoList;
