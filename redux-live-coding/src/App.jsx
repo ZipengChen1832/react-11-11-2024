4ed152b9e77381de575169876993d02359fbd4f1
wendywen20 <wenyimei6699@gmail.com>
2025-01-03 13:42:22 -0500

    gitMerge

--- redux-live-coding/src/App.jsx
+++ redux-live-coding/src/App.jsx
@@ -6,11 +6,13 @@ import React from 'react'
 
 function App() {
   return (
+    <React.Fragment> 
     <div>
       <h1>Redux</h1>
       <Counter />
       <TodoList/>
     </div>
+    </React.Fragment>
   );
 }
 
