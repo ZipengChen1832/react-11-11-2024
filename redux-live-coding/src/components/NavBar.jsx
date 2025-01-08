b3d0aa321e7be4ae6c4ff54445f5f4c87759d48c
beingjiang <96422819+beingjiang@users.noreply.github.com>
2025-01-08 13:24:06 -0500

    navbar-correction

--- redux-live-coding/src/components/NavBar.jsx
+++ redux-live-coding/src/components/NavBar.jsx
@@ -4,17 +4,9 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
           <Link to="/">Home</Link>
-        </li>
-        <li>
           <Link to="/todos">Incomplete Todos</Link>
-        </li>
-        <li>
           <Link to="/todos/completed">Completed Todos</Link>
-        </li>
-      </ul>
     </nav>
   );
 };
